a5d911bbd6a65af7602ea05a0d72345bd3c5d401
PannagaJA <pannaga.baradwaj@gmail.com>
2026-07-17 22:10:59 +0530

    feat: add wishlist-sync API handler for managing customer wishlist data in Supabase

--- api/wishlist-sync.js
+++ api/wishlist-sync.js
@@ -2,7 +2,11 @@ import { supabase } from "../lib/supabase.js";
 
 export default async function handler(req, res) {
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://klabo-3.myshopify.com"
+    
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
